cb3bb8703e2c434c82b761efff564b2ba8e0ba20
TjerkZ <lala@pepr.nl>
2023-11-02 00:34:12 +0100

    Project database settings

--- DAOInterfaces/DTO/ProjectDTO.cs
+++ DAOInterfaces/DTO/ProjectDTO.cs
@@ -1,7 +1,12 @@
-﻿namespace DAOInterfaces.DTO
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DAOInterfaces.DTO
 {
     public class ProjectDTO
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public required string Id { get; set; }
         public required string UserId {  get; set; }
         public required string Name { get; set; }
