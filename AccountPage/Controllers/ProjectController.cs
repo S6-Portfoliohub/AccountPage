8f5247f218edd5567b754a79bab8f694ca6596a0
TjerkZ <lala@pepr.nl>
2023-11-06 01:27:04 +0100

    making it work on docker

--- AccountPage/Controllers/ProjectController.cs
+++ AccountPage/Controllers/ProjectController.cs
@@ -1,18 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DAL.DAO;
+using AccountPage.Models;
+using System.Xml.Linq;
 
 namespace AccountPage.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProjectController : ControllerBase
     {
-        [HttpGet(Name = "GetAllProjectsFromUser")]
-        public async Task<IActionResult> GetAllProjectsFromUser(string userId)
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<List<ProjectViewModel>>> GetAllProjectsFromUser(string userId)
         {
-            ProjectDAL projectDAL = new();
-            projectDAL.GetAllProjectsFromUser(userId);
-            return Ok();
+            //ProjectDAL projectDAL = new();
+            //await projectDAL.GetAllProjectsFromUser(userId);
+            List<ProjectViewModel> project = new()
+            {
+                new() { Id = "test", Name = "test", Description = "test", Img = "test.png" },
+                new() { Id = "test1", Name = "test1", Description = "test1", Img = "test1.png" }
+            };
+
+            return project;
         }
     }
 }
--- AccountPage/Models/ProjectViewModel.cs
+++ AccountPage/Models/ProjectViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class ProjectViewModel
     {
-        public required int Id { get; set; }
+        public required string Id { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
         public string? Img {  get; set; }
